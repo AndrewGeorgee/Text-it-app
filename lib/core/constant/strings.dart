24b1fc16182dfd037d2454dfe2afaee71dc297ae
Andrew George <andrew.george.rage@gmail.com>
2024-06-02 17:17:31 +0300

    add some constant

---
+++ lib/core/constant/strings.dart
@@ -0,0 +1,23 @@
+class AppString {
+  static String connectFriendsEasilyQuickly =
+      "Connect friends easily & quickly";
+  static String ourChatAppIsThePerfectWayToStayConnectedWithFriendsAndFamily =
+      "Our chat app is the perfect way to stay connected with friends and family.";
+  static String signUpWithMail = "Sign up with mail";
+  static String or = "OR";
+  static String existingAccount = "Existing account? ";
+  static String login = "Log in";
+  static String logIntoChatbox = "Log in to Chatbox";
+  static String welcomeBack =
+      "Welcome back! Sign in using your social account or email to continue us";
+  static String yourEmail = "Your email";
+  static String password = "Password";
+  static String forgotPassword = "Forgot password?";
+  static String signUpWithEmail = "Sign up with Email";
+  static String yourName = "Your name";
+  static String confirmPassword = "Confirm Password";
+  static String createAnAccount = "Create an account";
+
+  static String getChattingWith =
+      "Get chatting with friends and family today by signing up for our chat app!";
+}
